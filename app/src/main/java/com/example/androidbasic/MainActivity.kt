fdfa1cc542ceab7557e6cef7ccdb92c87efa1e31
B. Arruda de Castro <b.arruda.de.castro@avanade.com>
2020-09-17 21:59:33 -0300

    Implementar soma de dois valores do editText

--- app/src/main/java/com/example/androidbasic/MainActivity.kt
+++ app/src/main/java/com/example/androidbasic/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.androidbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSoma.setOnClickListener {
+            var val1  = if (number1.text.toString().isNullOrEmpty()) 0 else number1.text.toString().toDouble()
+            var val2  = if (number2.text.toString().isNullOrEmpty()) 0 else number2.text.toString().toDouble()
+            resultSoma.text = "A soma é "+ somar(val1 as Double, val2 as Double)
+        }
     }
+
+    private fun somar(val1: Double, val2: Double) = val1.plus(val2)
 }
