51fdf3436bd7595ab6ae760ff3a4971c61981c3b
B. Arruda de Castro <b.arruda.de.castro@avanade.com>
2020-09-26 00:07:38 -0300

    implementar funcionalidade de calculo basico

--- app/src/main/java/com/example/androidbasic/MainActivity.kt
+++ app/src/main/java/com/example/androidbasic/MainActivity.kt
@@ -2,21 +2,56 @@ package com.example.androidbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonSoma.setOnClickListener {
-            var val1  = if (number1.text.toString().isNullOrEmpty()) 0 else number1.text.toString().toDouble()
-            var val2  = if (number2.text.toString().isNullOrEmpty()) 0 else number2.text.toString().toDouble()
-            resultSoma.text = "A soma é "+ somar(val1 as Double, val2 as Double)
+        /*add_event_button*/
+        add_button.setOnClickListener {
+            result_value.text = add(
+                checkNumberisNull(number_1.text.toString()),
+                checkNumberisNull(number_2.text.toString())
+            ).toString()
         }
+
+        /*subtract_event_button*/
+        subtract_button.setOnClickListener{
+            result_value.text = subtract(
+                checkNumberisNull(number_1.text.toString()),
+                checkNumberisNull(number_2.text.toString())
+            ).toString()
+        }
+
+        /*multiply_event_button*/
+        multiply_button.setOnClickListener {
+            result_value.text = multiply(
+                checkNumberisNull(number_1.text.toString()),
+                checkNumberisNull(number_2.text.toString())
+            ).toString()
+        }
+
+        /*divide_event_button*/
+        divide_button.setOnClickListener {
+            result_value.text = divide(
+                checkNumberisNull(number_1.text.toString()),
+                checkNumberisNull(number_2.text.toString())
+            ).toString()
+        }
+
     }
 
-    private fun somar(val1: Double, val2: Double) = val1.plus(val2)
+    private fun add(val1: Double, val2: Double) = val1.plus(val2)
+
+    private fun subtract(val1: Double, val2: Double) = val1.minus(val2)
+
+    private fun multiply(val1: Double, val2: Double) = val1.times(val2)
+
+    private fun divide(val1: Double, val2: Double) = val1.div(val2)
+
+    private fun checkNumberisNull(numberValue: String): Double {
+        return if (numberValue.isNullOrEmpty()) 0.0 else numberValue.toDouble()
+    }
 }
